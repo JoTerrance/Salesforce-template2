bbebfa8159eec40b1690e2a4994948c9923a63e6
Gorka Sanz <gorka.sanz@salesianos.edu>
2023-01-18 13:01:07 +0100

    trying to call an apex class from aura component

--- force-app/main/default/aura/datos/datosController.js
+++ force-app/main/default/aura/datos/datosController.js
@@ -7,16 +7,29 @@
         console.log(num1);
         var num2 = component.get("v.num2");
         console.log(num2);
-        var resultado = num1 + num2;
-        component.set("v.resultado", resultado);
+        var sumador = component.get("c.sumador");
+        sumador.setCallback(function(response){
+            var state = response.getState();
+            if(state === "SUCCESS"){
+                resultado = response.getReturnValue();
+                component.set("v.resultado", resultado);
+            }
+        });
+        sumador.setParams({a : num1, b : num2});
+        $A.enqueueAction(sumador);
+        //var resultado = num1 + num2;
+        //component.set("v.resultado", resultado);
     },
-    saveNum1 : function(component, event, helper) {
-        var num1 = component.find("input1").get("v.value");
+    saveNum1 : function(component, event, helper) 
+    {   
+        var l_input =component.find("input0").get("v.value");
+        console.log(l_input);
+        var num1 = component.find("input1").getElement().value;
         component.set("v.num1", num1);
         console.log(num1);
     },
     saveNum2 : function(component, event, helper) {
-        var num2 = component.find("input2").get("v.value");
+        var num2 = component.find("input2").getElement().value;
         component.set("v.num2", num2);
         console.log(num2);
     }
